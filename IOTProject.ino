247d6ffe0b0fc32fac0beb7c076f38a254abf8b4
gettinschwifty1 <157513662+gettinschwifty1@users.noreply.github.com>
2024-04-30 14:48:03 +0100

    working final demo code

--- IOTProject.ino
+++ IOTProject.ino
@@ -14,44 +14,36 @@
 /////////////////
 
 
-char devid = "v519BB414074997D";        //Scenario : "The mailbox is open"
-
-//Numeric Pin where you connect your switch
-
-
-// Debug mode
-
+// Add your PushingBox Scenario DeviceID here:
+  char devid[] = "v519BB414074997D";
+  
   //////////////
  //   End    //
 //////////////
 
 char serverName[] = "api.pushingbox.com";
+boolean DEBUG = true;
+#include <Bridge.h>
+#include <HttpClient.h>
 
-
-//--------------------------------------------------------// */
-bool DEBUG = true;
-//  code for reading sensor originates from https://sensorkit.arduino.cc/sensorkit/module/lessons/lesson/05-the-light-sensor , but is modified
-int lightLevel = 7; 
-int light_sensor = A3; 
-bool timer = false;
-
-void setup() 
-{
-  Serial.begin(9600); //begin Serial Communication
-  
-  
-  // set up for pushingbox notifactions
-  //---------------------------------------------------------------------------------------------------------------------------------
-  //initialize serial:
-  //Serial.begin(9600);
+void setup() {
+  // Bridge takes about two seconds to start up
+  // it can be helpful to use the on-board LED
+  // as an indicator for when it has initialized
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
   Bridge.begin();
   digitalWrite(13, HIGH);
+
+  Serial.begin(9600);
   while (!Serial); // wait for a serial connection
- 
- 
 }
+//  code for reading sensor originates from https://sensorkit.arduino.cc/sensorkit/module/lessons/lesson/05-the-light-sensor , but is modified
+int lightLevel = 10; 
+int light_sensor = A3; 
+bool timer = false;
+
+
 void loop() 
 {
   // put your main code here, to run repeatedly:
@@ -96,18 +88,17 @@ void loop()
     if (count >= 3) 
     {
       digitalWrite(5, HIGH);
-      //timer = false;
-      //Serial.print("");
-     // sendToPushingBox(DEVID1);
+      
       String APIRequest;
       APIRequest = String(serverName) + "/pushingbox?devid=" + String(devid)+ "&IDtag=10000&TimeStamp=50&TempC="+light;
       client.get (APIRequest);
-      while (client.available()) {
-    char c = client.read();
+      while (client.available()) 
+      {
+        char c = client.read();
 
-  }
-      //loopWifiSetup();
-        Serial.flush();
+      }
+      
+      Serial.flush();
       String UploadMessage;
       
     }
