f898ab4388512ff68ff8f00582fda61d5d228b20
gettinschwifty1 <157513662+gettinschwifty1@users.noreply.github.com>
2024-04-30 11:21:43 +0100

    Add files via upload

--- IOTProject.ino
+++ IOTProject.ino
@@ -1,8 +1,6 @@
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <WiFiServer.h>
-#include <WiFiUdp.h>
-
+#include <Bridge.h>
+#include <HttpClient.h>
+//#include <PushingBox-for-Arduino-WiFi-shield.ino>
 
 
 
@@ -14,30 +12,26 @@
   /////////////////
  // MODIFY HERE //
 /////////////////
-char wifissid[] = "WIFI_SSID";   //  your network SSID (name)
-char wifipass[] = "doghunter";    // your WPA network password
 
-char DEVID1[] = "v519BB414074997D";        //Scenario : "The mailbox is open"
+
+char devid = "v519BB414074997D";        //Scenario : "The mailbox is open"
 
 //Numeric Pin where you connect your switch
-uint8_t pinDevid1 = 3; // Example : the mailbox switch is connect to the Pin 3
+
 
 // Debug mode
-boolean DEBUG = true;
+
   //////////////
  //   End    //
 //////////////
 
 char serverName[] = "api.pushingbox.com";
-boolean pinDevid1State = false;                // Save the last state of the Pin for DEVID1
-boolean lastConnected = false;  
-int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
-WiFiClient client;
-//--------------------------------------------------------// 
 
-//  code for reading sensor comes from https://sensorkit.arduino.cc/sensorkit/module/lessons/lesson/05-the-light-sensor
- 
+//--------------------------------------------------------// */
+bool DEBUG = true;
+//  code for reading sensor originates from https://sensorkit.arduino.cc/sensorkit/module/lessons/lesson/05-the-light-sensor , but is modified
+int lightLevel = 7; 
 int light_sensor = A3; 
 bool timer = false;
 
@@ -45,115 +39,96 @@ void setup()
 {
   Serial.begin(9600); //begin Serial Communication
   
+  
   // set up for pushingbox notifactions
   //---------------------------------------------------------------------------------------------------------------------------------
-  // initialize serial:
-  Serial.begin(9600);
-  pinMode(pinDevid1, INPUT);
+  //initialize serial:
+  //Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  Bridge.begin();
+  digitalWrite(13, HIGH);
+  while (!Serial); // wait for a serial connection
  
-  // attempt to connect using WPA2 encryption:
-  Serial.println("Attempting to connect to WPA network...");
-  status = WiFi.begin(wifissid, wifipass);
  
-  // if you're not connected, stop here:
-  if ( status != WL_CONNECTED) {
-    Serial.println("Couldn't get a wifi connection");
-    while(true);
-  }
-  // if you are connected, print out info about the connection:
-  else {
-    Serial.println("Connected to network");
-  }
-  //----------------------------------------------------------------------------------------------------------------------------------
 }
- 
 void loop() 
 {
+  // put your main code here, to run repeatedly:
   int raw_light = analogRead(light_sensor); // read the raw value from light_sensor pin (A3)
   int light = map(raw_light, 0, 1023, 0, 100); // map the value from 0, 1023 to 0, 100
   int ledPin = 5;
+
+  HttpClient client;
 
   Serial.print("Light level: "); 
   Serial.println(light); // print the light value in Serial Monitor
  
   delay(100); // add a delay to only read and print every 100 miliseconds or tenth of a second
   
-  
-  if(light < 10 )
+  //Serial.flush();
+  //String UploadMessage;
+  //Serial.print("\n Uploaded temp value: ");
+ // Serial.print(light);
+  //delay(5000);
+  if(light < lightLevel )
   {
    //digitalWrite(5, LOW);
    timer = true;
   } 
-  
   else 
   {
     // turn LED off:
     digitalWrite(5, LOW);
-    timer == false;
+    timer = false;
   }    
+  // printing if the timer is activated - 0 is no, 1 is yes
   Serial.print("Timer: "); 
-  Serial.print(timer);
-        // deivydosser is dossing again 06.03.2024, 19.03.2024, 16.04.2024, 23.04.2024,  
-  int cnt = 0; // counter that is set to go up by one every second
-  while(timer == true && light < 10)// while light is less then 10 
+  Serial.print(timer );
+
+  // deivydosser is dossing again 06.03.2024, 19.03.2024, 16.04.2024, 23.04.2024, 
+
+  int count = 0; // counter that is set to go up by one every second
+  while(timer == true && light < lightLevel)// while light is less then specified level
   {
     delay(1000);
-    cnt++;
-    if (cnt >= 10) 
+    count++;
+    if (count >= 3) 
     {
       digitalWrite(5, HIGH);
-      timer = false;
-      sendToPushingBox(DEVID1);
+      //timer = false;
+      //Serial.print("");
+     // sendToPushingBox(DEVID1);
+      String APIRequest;
+      APIRequest = String(serverName) + "/pushingbox?devid=" + String(devid)+ "&IDtag=10000&TimeStamp=50&TempC="+light;
+      client.get (APIRequest);
+      while (client.available()) {
+    char c = client.read();
+
+  }
+      //loopWifiSetup();
+        Serial.flush();
+      String UploadMessage;
+      
     }
+    
+    
+      
+    
     raw_light = analogRead(light_sensor); // read the raw value from light_sensor pin (A3)
     light = map(raw_light, 0, 1023, 0, 100);
     
-    Serial.print("Counter: "); 
-    Serial.print(cnt);
+    Serial.print("\nCounter: "); // printing the amount of time the light sensor has been covered in seconds
+    Serial.print(count);
+    
   }
 }
-//-----------------------------------------------------------------------------------------------------------------------------------------------
-void loopWifiSetup() //taken from pushingbox code for setting up
-{
-   ////
-   // Listening for the pinDevid1 state
-   ////
-  if (digitalRead(pinDevid1) == HIGH && pinDevid1State == false) // switch on pinDevid1 is ON
-  {
-    if(DEBUG){Serial.println("pinDevid1 is HIGH");}
-    pinDevid1State = true;
-    //Sending request to PushingBox when the pin is HIGHT
-    sendToPushingBox(DEVID1);
-  }
-   if (digitalRead(pinDevid1) == LOW && pinDevid1State == true) // switch on pinDevid1 is OFF
-  {
-    if(DEBUG){Serial.println("pinDevid1 is LOW");}
-    pinDevid1State = false;
-    //Sending request to PushingBox when the pin is LOW
-    //sendToPushingBox(DEVID1);    //Here you can run an other scenario by creating a DEVID2 variable
-  }
- 
-  //DEBUG part
-  // this write the respons from PushingBox Server.
-  // You should see a "200 OK"
-  if (client.available()) {
-    char c = client.read();
-    if(DEBUG){Serial.print(c);}
-  }
- 
-  // if there's no net connection, but there was one last time
-  // through the loop, then stop the client:
-  if (!client.connected() && lastConnected) {
-    if(DEBUG){Serial.println();}
-    if(DEBUG){Serial.println("disconnecting.");}
-    client.stop();
-  }
-  lastConnected = client.connected();
-}
-//Function for sending the request to PushingBox
+
+/*/Function for sending the request to PushingBox
 void sendToPushingBox(char devid[]){
-  client.stop(); if(DEBUG){Serial.println("connecting...");}
-  if(client.connect(serverName, 80)) { 
+  client.stop();
+  if(DEBUG){Serial.println("connecting...");}
+  if(client.connect(serverName, pinDevid1)) { 
     if(DEBUG){Serial.println("connected");}
     if(DEBUG){Serial.println("sendind request");}
     client.print("GET /pushingbox?devid=");
@@ -167,6 +142,5 @@ void sendToPushingBox(char devid[]){
   else { 
     if(DEBUG){Serial.println("connection failed");} 
   } 
-}
-
-
+  return;
+}*/
